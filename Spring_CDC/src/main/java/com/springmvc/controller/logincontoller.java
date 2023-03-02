d8e295153f7815818eb826bc48f8416365f425c2
siljae <96092314+siljae@users.noreply.github.com>
2023-03-02 20:25:44 +0900

    20230302
    2025

--- Spring_CDC/src/main/java/com/springmvc/controller/logincontoller.java
+++ Spring_CDC/src/main/java/com/springmvc/controller/logincontoller.java
@@ -1,8 +1,15 @@
 package com.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.springmvc.domain.memberDTO;
 
 @Controller
 @RequestMapping("/login")
@@ -14,7 +21,17 @@ public class logincontoller {
 	}
 	
 	@GetMapping("/signup")
-	public String signup() {
+	public String signup(@ModelAttribute("member") memberDTO member) {
 		return "signup";
+	}
+	
+	@PostMapping("/signup")
+	public String join(@ModelAttribute("member") memberDTO member) {
+		return "signup2";
+	}
+	
+	@InitBinder
+	public void initBinde(WebDataBinder binder) {
+		binder.setAllowedFields("email","name","pw","phone1","phone2","phone3","post","addr1","addr2");
 	}
 }
