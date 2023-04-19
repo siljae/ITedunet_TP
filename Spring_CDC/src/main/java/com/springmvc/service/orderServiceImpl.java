8b31049922910768452ba67303236933452ffb76
siljae <96092314+siljae@users.noreply.github.com>
2023-04-20 00:28:44 +0900

    Update orderServiceImpl.java

--- Spring_CDC/src/main/java/com/springmvc/service/orderServiceImpl.java
+++ Spring_CDC/src/main/java/com/springmvc/service/orderServiceImpl.java
@@ -13,21 +13,29 @@ public class orderServiceImpl implements orderService {
    @Autowired
    private orderRepository or;
    
-   public void setNewOrder(String productId, String name, int totalPrice, orderDTO order, String onum) {
+   //주문목록 추가
+   public void setNewOrder(String productname, String name, int totalPrice, orderDTO order, String onum) {
 	   System.out.println("orderservice setNewOrder 들어왔댜"); 
 	   System.out.println("orderservice - name :" + name);
-       or.setNewOrder(productId, name, totalPrice, order, onum);
+       or.setNewOrder(productname, name, totalPrice, order, onum);
    }
    
+   // 주문목록 전체 리스트
    public List<orderDTO> getAllOrderList(String name){
 	   System.out.println("orderservice AllOrderList 들어왔댜"); 
        return or.getAllOrderList(name);
    }
    
+   // 같은 이름인거 찾아서 반환
    public orderDTO getMemberByName(String name) {
 	  System.out.println("orderservice MemberByName 들어왔댜"); 
       orderDTO MemberByName = or.getMemberByNmae(name);
       return MemberByName;
+   }
+   
+   // 주문목록 상세보기 리스트
+   public List<orderDTO> getOrderprolist(String onum){
+	   return or.getOrderprolist(onum);
    }
    	
 }
