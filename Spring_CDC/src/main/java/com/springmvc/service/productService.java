65bf8e61bec2e32b6e8f3fb7314e1efb75359efb
siljae <96092314+siljae@users.noreply.github.com>
2023-04-20 00:28:56 +0900

    Update productService.java

--- Spring_CDC/src/main/java/com/springmvc/service/productService.java
+++ Spring_CDC/src/main/java/com/springmvc/service/productService.java
@@ -2,13 +2,22 @@ package com.springmvc.service;
 
 import java.util.List;
 
+import org.springframework.ui.Model;
+
 import com.springmvc.domain.productDTO;
 
 public interface productService {
-	List<productDTO> getAllProductList();
-//	List<productDTO> getProductListByCategory(String category);
+	//상품전체리스트
+	List<productDTO> getAllProductList(); 
+	//상품아이디와 일치하는 상품 반환
 	productDTO getProductById(String productId);
-	void setNewProduct(productDTO product);
+	//상품추가
+	void setNewProduct(productDTO product); 
+	//상품수정
 	void setUpdateProduct(productDTO product);
-	void setDeleteProduct(String productId);
+	//상품삭제
+	void setDeleteProduct(String productId); 
+	
+//	public void getallproduct(Model model);
+//	public void getproductByid(String productId, Model model);
 }
