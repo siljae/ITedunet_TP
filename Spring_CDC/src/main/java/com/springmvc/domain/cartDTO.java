49642acfdee3b41674a3d5812244df69e9aad55b
siljae <96092314+siljae@users.noreply.github.com>
2023-04-20 00:23:47 +0900

    Update cartDTO.java

--- Spring_CDC/src/main/java/com/springmvc/domain/cartDTO.java
+++ Spring_CDC/src/main/java/com/springmvc/domain/cartDTO.java
@@ -1,16 +1,15 @@
 package com.springmvc.domain;
 
-import java.util.List;
 
 public class cartDTO {
    
    private int cartId;
    private String name;
    private String productId;
+   private String productname;
    private int price;
    private int quantity;
    private String tfilename;
-   private List<cartitemDTO> cartitem;
    
    public int getCartId() {
       return cartId;
@@ -30,9 +29,16 @@ public class cartDTO {
    public void setProductId(String productId) {
       this.productId = productId;
    }
-   public int getPrice() {
-      return price;
+   
+   public String getProductname() {
+	return productname;
    }
+	public void setProductname(String productname) {
+		this.productname = productname;
+	}
+	public int getPrice() {
+	      return price;
+	}
    public void setPrice(int price) {
       this.price = price;
    }
@@ -48,12 +54,7 @@ public class cartDTO {
    public void setTfilename(String tfilename) {
       this.tfilename = tfilename;
    }
-	public List<cartitemDTO> getCartitem() {
-		return cartitem;
-	}
-	public void setCartitem(List<cartitemDTO> cartitem) {
-		this.cartitem = cartitem;
-	}
+
    
    
       
